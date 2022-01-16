90e216ca6bd0549dbe3b5eb68345429253e347d9
Kaan Genc <genc.5@osu.edu>
2022-01-16 02:39:10 -0500

    doesn't work

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,49 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 mod fairings;
 use fairings::logger::Logger;
-use fairings::database::{Db};
+use fairings::database::Db;
 use tracing::Level;
-use sea_orm_rocket::{Database};
+use sea_orm_rocket::{Database, Connection};
 
-#[macro_use]
-extern crate rocket;
-
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+use rocket_contrib::json::{Json, JsonValue};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[derive(Serialize, Deserialize)]
+enum TokenType {
+    ANON,
+    VIEW,
+    MOD,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Token {
+    token: String,
+    tokenType: TokenType,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Login {
+    username: String,
+    password: String,
+}
+
+type LoginAttempt = Option<Login>;
+
+
+#[post("/auth", format = "json", data = "<login>")]
+fn auth(conn: Connection<'_, Db>, login: Json<Login>) -> JsonValue {
+    json!(Token {
+        token: login.username,
+        tokenType: TokenType::VIEW
+    })
 }
 
 #[launch]
@@ -21,5 +54,5 @@ async fn rocket() -> _ {
     rocket::build()
         .attach(logger)
         .attach(Db::init())
-        .mount("/", routes![index])
+        .mount("/", routes![index, auth])
 }
